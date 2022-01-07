65d6b987dd768b6f95c47c7b782bdeab6233432f
agostino <agostino.dinuzzo@icloud.com>
2022-01-07 16:04:13 +0100

    added buttons and functions

--- README.md
+++ README.md
@@ -1,11 +1,24 @@
 # Cocktail-dbJS
-### random cocktail generator
-Vanilla JavaScript to create an app as an exercise
- - [cocktailDB](https://www.thecocktaildb.com/)
 
+### random cocktail generator
+
+Vanilla JavaScript to create an CocktailDB
+
+## JSON-SERVER
+
+Per far si che l'applicazione funzioni correttamente avviare json-server
+
+```
+json-server db.json
+```
+
+- [JSON db](https://www.npmjs.com/package/node-json-db)
+
+## COCKTAIL DB
+
+- [cocktailDB](https://www.thecocktaildb.com/)
 
 ## Screenshots
 
 ![App Screenshot](./assets/app1.png)
 ![App Screenshot](./assets/app.png)
-
--- main.js
+++ main.js
@@ -1,12 +1,16 @@
+// ======================== *
+// GET COCKTAIL 
+// ======================== *
 let urlImg, cocktailName, alcoholic, description;
 const change = document.getElementById("change");
+change.addEventListener("click", () => getCockail());
 
 async function getCockail() {
     try {
         let url = `https://www.thecocktaildb.com/api/json/v1/1/random.php`;
         const response = await fetch(url);
         const data = await response.json();
-        console.log(data);
+        // console.log(data);
         urlImg = data.drinks.map(drink => drink.strDrinkThumb);
         cocktailName = data.drinks.map(drink => drink.strDrink);
         alcoholic = data.drinks.map(drink => drink.strAlcoholic);
@@ -22,4 +26,52 @@ async function getCockail() {
 }
 getCockail();
 
-change.addEventListener("click", () => getCockail());
+// ======================== *
+// ADD COCKTAIL
+// ======================== *
+let ordersList = new Array();
+const addCocktail = document.getElementById("add");
+
+addCocktail.addEventListener("click", () => {
+    let newArrayOrders = [...ordersList, cocktailName[0]];
+    ordersList = newArrayOrders;
+    orderDom.innerText = "Ordine: " + ordersList;
+    return ordersList;
+});
+
+// ======================== *
+// BUY COCKTAIL 
+// ======================== *
+const buyButton = document.getElementById("buy");
+const orderDom = document.getElementById("order");
+let data = localStorage.getItem("order", ordersList);
+
+buyButton.addEventListener("click", async () => {
+    try {
+        fetch("http://localhost:3000/cocktailsOrder", {
+            method: "POST",
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(ordersList)
+        }).then((res) => {
+            console.log(res);
+            if (res.status >= 400 && res.status <= 451) {
+                throw new Error("Client responses " + res.status + ' ' + res.statusText)
+            } else if (500 >= res.status >= 511) {
+                throw new Error("Server responses " + res.status + ' ' + res.statusText)
+            } else if (res.ok === true) {
+                localStorage.setItem("order", ordersList);
+            }
+        })
+    } catch (error) {
+        console.error(error)
+    };
+});
+
+if (data.length > 0 && data !== null) {
+    orderDom.innerText = "Ordine effettuato: " +
+        localStorage.getItem("order", ordersList);
+} else {
+    orderDom.innerText = "Effettua il tuo ordine!"
+}
