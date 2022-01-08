c3b2a443b86a21cd8ed348497bf2bb47fc9ad105
agostino <agostino.dinuzzo@icloud.com>
2022-01-08 03:36:08 +0100

    improved code

--- main.js
+++ main.js
@@ -1,33 +1,54 @@
-// ======================== *
-// GET COCKTAIL 
-// ======================== *
-let urlImg, cocktailName, alcoholic, description;
-const change = document.getElementById("change");
-change.addEventListener("click", () => getCockail());
+// ======================== * 
+// GET DATA
+// ======================== * 
+let urlImg, cocktailName, alcoholic, description, ingredients = [];
 
 async function getCockail() {
-    try {
-        let url = `https://www.thecocktaildb.com/api/json/v1/1/random.php`;
-        const response = await fetch(url);
-        const data = await response.json();
-        // console.log(data);
-        urlImg = data.drinks.map(drink => drink.strDrinkThumb);
-        cocktailName = data.drinks.map(drink => drink.strDrink);
-        alcoholic = data.drinks.map(drink => drink.strAlcoholic);
-        description = data.drinks.map(drink => drink.strInstructionsIT);
-    } catch (error) {
-        console.error(error);
-    }
+    let url = `https://www.thecocktaildb.com/api/json/v1/1/random.php`;
+    const response = await fetch(url);
+    const data = await response.json();
+    // console.log(data);
+
+    urlImg = data.drinks.map(drink => drink.strDrinkThumb);
+    cocktailName = data.drinks.map(drink => drink.strDrink);
+    alcoholic = data.drinks.map(drink => drink.strAlcoholic);
+    description = data.drinks.map(drink => drink.strInstructionsIT);
+    ingredients = data.drinks.map(drink => [
+        drink.strIngredient1,
+        drink.strIngredient2,
+        drink.strIngredient3,
+        drink.strIngredient4,
+        drink.strIngredient5,
+        drink.strIngredient6,
+        drink.strIngredient7,
+        drink.strIngredient8,
+        drink.strIngredient9,
+        drink.strIngredient10,
+        drink.strIngredient11,
+        drink.strIngredient12,
+        drink.strIngredient13,
+        drink.strIngredient14,
+        drink.strIngredient15
+    ]);
+    ingredients = ingredients[0].filter(ingredient => ingredient !== null);
+
 
     document.getElementById('cocktailImg').setAttribute('src', urlImg);
     document.getElementById('name').innerText = cocktailName;
     document.getElementById('alcoholic').innerText = alcoholic;
+    document.getElementById('ingredients').innerText = ingredients;
     document.getElementById('description').innerText = description;
 }
-getCockail();
+getCockail().catch(e => console.error("Si è verificato un errore (GET METHOD) " + e));
+
+// ======================== * 
+// (CHANGE) BUTTON
+// ======================== * 
+const change = document.getElementById("change");
+change.addEventListener("click", () => getCockail());
 
 // ======================== *
-// ADD COCKTAIL
+// (ADD) BUTTON
 // ======================== *
 let ordersList = new Array();
 const addCocktail = document.getElementById("add");
@@ -40,7 +61,7 @@ addCocktail.addEventListener("click", () => {
 });
 
 // ======================== *
-// BUY COCKTAIL 
+// (BUY) BUTTON 
 // ======================== *
 const buyButton = document.getElementById("buy");
 const orderDom = document.getElementById("order");
@@ -55,9 +76,11 @@ buyButton.addEventListener("click", async () => {
             },
             body: JSON.stringify(ordersList)
         }).catch((e) => {
-            console.error("Assicurati di essere connesso al Database 'db.json' " + e);
+            console.error("Assicurati di essere connesso al Database (db.json) " + e);
         }).then((res) => {
-            if (res.status >= 400 && res.status <= 451) {
+            if (res === undefined) {
+                throw new Error("Il server non è connesso");
+            } else if (res.status >= 400 && res.status <= 451) {
                 throw new Error("Errore di risposta Client " + res.status + ' ' + res.statusText);
             } else if (500 >= res.status >= 511) {
                 throw new Error("Errore di risposta Server " + res.status + ' ' + res.statusText);
@@ -65,7 +88,7 @@ buyButton.addEventListener("click", async () => {
                 localStorage.setItem("order", ordersList);
             }
         }).catch((e) => {
-            console.error("Si è verificato un errore " + e);
+            console.error("POST METHOD: " + e);
         })
     } catch (error) {
         console.error(error);
